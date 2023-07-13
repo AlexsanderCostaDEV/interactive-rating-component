fa8d31793eb123081213e87640ac5e075ebee49f
AlexsanderCostaDEV <alexsander0souza@gmail.com>
2023-07-13 07:37:43 -0400

    project finished

--- js/scritp.js
+++ js/scritp.js
@@ -1,21 +1,33 @@
+const mainContainer = document.querySelector(".card");
+const thanksContainer = document.querySelector(".card2");
+const submitButton = document.getElementById("submit");
+const rating = document.getElementById("rating");
+const rates = document.querySelectorAll(".change");
 var links = document.querySelectorAll('.change');
-    var changeColor = null;
+var changeColor = null;
 
-    for (var i = 0; i < links.length; i++) {
-        links[i].addEventListener('click', function() {
-        // Restore original color of previous modified button
-        if (changeColor) {
-          changeColor.classList.remove('selected');
-        }
-
-        // Modify the color of the clicked button
-        this.classList.add('selected');
-
-        // Store reference to currently modified button
-        changeColor = this;
-      });
+for (var i = 0; i < links.length; i++) {
+    links[i].addEventListener('click', function() {
+    // Restore original color of previous modified button
+    if (changeColor) {
+      changeColor.classList.remove('selected');
     }
 
-function redirect() {
-    window.location.href = './thank-you.html';
+    // Modify the color of the clicked button
+    this.classList.add('selected');
+
+    // Store reference to currently modified button
+    changeColor = this;
+  });
 }
+
+submitButton.addEventListener("click", () => {
+    thanksContainer.classList.remove("hidden");
+    mainContainer.style.display = "none";
+})
+
+rates.forEach((rate) => {
+    rate.addEventListener("click", () => {
+    rating.innerHTML = rate.innerHTML;
+  })
+})
